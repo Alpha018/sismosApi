e883d2cfd398946d05c9eb284892fb1eca26158f
alpha018 <tas001@alumnos.ucn.cl>
2018-11-09 13:14:39 -0300

    Fixed: se implementa la API

--- server.js
+++ server.js
@@ -15,39 +15,17 @@ mongoose.connect('mongodb://@localhost:27017/sismos', {useNewUrlParser: true})
     .then(() => {
         console.log('La conexion a la base de datos se a realizado Correctamente');
 
-        /*app.listen(port, () => {
+        app.listen(port, () => {
             console.log('El servidor esta corriendo en el puerto: 8000')
-        })*/
+        });
 
-        const format = require('date-format');
-        const sismo = require('./server/model/sismo');
-        const moment = require('moment');
-
-        sismo.find({}, (err, listaSismos) => {
-            for(let i = 0; i < listaSismos.length; i++) {
-
-                const aux = listaSismos[i].horaLocal.split(' ');
-                const date = aux[1].split('/');
-                const fecha = moment(aux[0] + " " + date[1] + "/" + date[0] + "/" + date[2]).toDate();
-
-                listaSismos[i].localDate = fecha;
-
-                listaSismos[i].save((err, sismoG) => {
-                    if (err) {
-                        console.log(err)
-                    } else {
-                        console.log('sismo guardado')
-                    }
-                })
-            }
-        })
-        /*const task = cron.schedule('10 * * * * *', () =>  {
+        const task = cron.schedule('10 * * * * *', () =>  {
             api.getData();
         }, {
             scheduled: false
         });
 
-        task.start();*/
+        task.start();
 
     })
     .catch(err => console.log(err));
